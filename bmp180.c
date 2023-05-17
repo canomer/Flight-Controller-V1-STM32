7f39b33c3822a31cf6a995c38618a3012dd6f0da
Ömer Can <can.omer.5306@outlook.com>
2023-05-17 15:38:46 +0300

    Update bmp180.c

--- bmp180.c
+++ bmp180.c
@@ -2,7 +2,7 @@
  * bmp180.c
  *
  *  Created on: Feb 17, 2022
- *      Author: Kazim YEL
+ *      Author: Ömer Can VURAL
  * 
  *  
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation,

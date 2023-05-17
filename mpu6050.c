fc5872480a64a4a4585c8eac3d6b0c320c0d2877
Ömer Can <can.omer.5306@outlook.com>
2023-05-17 15:39:25 +0300

    Update mpu6050.c

--- mpu6050.c
+++ mpu6050.c
@@ -2,7 +2,7 @@
  * mpu6050.c
  *
  *  Created on: Dec 30, 2021
- *      Author: Kazim YEL
+ *      Author: Ömer Can VURAL
  * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
